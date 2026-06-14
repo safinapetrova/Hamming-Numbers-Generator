47722b7bf188d450166929c10a14a472da2b8603
safinapetrova <petrovasafina07@gmail.com>
2026-06-14 19:38:22 +0500

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,24 @@
+"""Модуль с пользовательскими исключениями для программы."""
+
+
+class HammingError(Exception):
+    """Базовое исключение для ошибок генератора Хэмминга."""
+    pass
+
+
+class InvalidCountError(HammingError):
+    """Исключение для некорректного количества чисел."""
+
+    def __init__(self, invalid_count_value):
+        """Инициализация исключения."""
+        self.invalid_count_value = invalid_count_value
+        HammingError.__init__(
+            self,
+            f"Некорректное количество чисел: {invalid_count_value}. "
+            f"Ожидается натуральное число (n >= 1)"
+        )
+
+
+class QueueError(HammingError):
+    """Исключение для ошибок работы с очередью."""
+    pass
